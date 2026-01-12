8a1f4a92ad329e81c3cbacc1d038b3f2942e116d
Sol <sodanabe@gmail.com>
2026-01-12 09:40:49 +0200

    async syntax

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -1,15 +1,16 @@
 const {Contact} = require("../models/contact");
 
-const showContacts = (req, res) => {
-    return Contact.findAll()
-        .then((contacts) => res.send(contacts))
-        .catch((err) => {
-            console.log('There was an error querying contacts', JSON.stringify(err))
-            err.error = 1; // some error code for client side
-            return res.status(400).send(err) // send the error to the client
-        });
-}
+const showContacts = async (req, res) => {
+    try {
+        const contacts = await Contact.findAll();
+        return res.send(contacts);
+    } catch (err) {
+        console.log('There was an error querying contacts', JSON.stringify(err))
+        err.error = 1; // some error code for client side
+        return res.status(400).send(err) // send the error to the client
+    }
 
+}
 const showContact = (req, res) => {
     return Contact.findAll({where: {lastName: req.params.lastName}})
         .then((contacts) => res.send(contacts))
@@ -29,29 +30,29 @@ const addContact = (req, res) => {
         })
 }
 
-const deleteContact = (req, res) => {
-    const id = parseInt(req.params.id);
-    return Contact.findByPk(id)
-        .then((contact) => contact.destroy({ force: true }))
-        .then(() => res.status(204).send())
-        .catch((err) => {
-            console.log('***Error deleting contact', JSON.stringify(err))
-            res.status(400).send(err)
-        })
+const deleteContact = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const contact = await Contact.findByPk(id);
+        await contact.destroy({ force: true });
+        return res.status(204).send();
+    } catch (err) {
+        console.log('***Error deleting contact', JSON.stringify(err))
+        return res.status(400).send(err)
+    }
 }
 
-const updateContact = (req, res) => {
-    const id = parseInt(req.params.id);
-    return Contact.findByPk(id)
-        .then((contact) => {
-            const { firstName, lastName, phone } = req.body
-            return contact.update({ firstName, lastName, phone })
-                .then(() => res.send(contact))
-                .catch((err) => {
-                    console.log('***Error updating contact', JSON.stringify(err))
-                    res.status(400).send(err)
-                })
-        })
+const updateContact = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const contact = await Contact.findByPk(id);
+        const { firstName, lastName, phone } = req.body
+        await contact.update({ firstName, lastName, phone });
+        return res.send(contact);
+    } catch (err) {
+        console.log('***Error updating contact', JSON.stringify(err))
+        return res.status(400).send(err)
+    }
 }
 
 
